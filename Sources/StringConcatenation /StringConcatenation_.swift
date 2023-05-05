8c9a4c7d34da6b3f90c3e7e541ddfb697d40a202
yogesh-hastree <yogeshkumar.hastree@gmail.com>
2023-05-05 17:42:15 +0530

    use protocol to hide the data

--- Sources/StringConcatenation /StringConcatenation_.swift
+++ Sources/StringConcatenation /StringConcatenation_.swift
@@ -1,10 +1,26 @@
-internal struct StringConcatenation_ {
-    internal private(set) var text = "Hello, World!"
-
-    internal init() {
-    }
-    
-    internal func concatenateStrings(_ str1: String, _ str2: String) -> String {
-        return str1 + " " + str2
-    }
+protocol Abstraction{
+    func concatenateStrings(_ str1: String, _ str2: String) -> String
 }
+
+//class PackageClass: Abstraction{
+//    func concatenateStrings(_ str1: String, _ str2: String) -> String {
+//        return str1 + " " + str2
+//    }
+    
+
+    internal struct StringConcatenation_: Abstraction{
+        func concatenateStrings(_ str1: String, _ str2: String) -> String {
+            return str1 + " " + str2
+        }
+        
+        //    var delegate: Abstraction?
+        
+        internal private(set) var text = "Hello, World!"
+        
+        internal init() {
+        }
+        
+        //     func concatenateStrings(_ str1: String, _ str2: String) -> String {
+        //        return str1 + " " + str2
+        //    }
+    }
